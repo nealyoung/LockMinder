28fe9bc2e25eee0562ff9a40ff07cd18fbf57550
Nealon Young <hi@nealyoung.me>
2015-08-10 15:17:30 -0700

    Added image saving from preview view controller

--- LockMinder/ImagePreviewViewController.swift
+++ LockMinder/ImagePreviewViewController.swift
@@ -6,7 +6,8 @@
 //  Copyright (c) 2015 Nealon Young. All rights reserved.
 //
 
-import Foundation
+import AssetsLibrary
+import NYAlertViewController
 
 class ImagePreviewViewController: UIViewController {
     var image: UIImage?
@@ -14,6 +15,8 @@ class ImagePreviewViewController: UIViewController {
     private var imageView: UIImageView!
     private var cancelButton: UIButton!
     private var saveButton: UIButton!
+    
+    let savedPhotosAlbumName = "LockMinder Wallpapers"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +40,7 @@ class ImagePreviewViewController: UIViewController {
         self.saveButton.titleLabel?.font = UIFont.mediumApplicationFont(19.0)
         self.saveButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
         self.saveButton.setTitle("Save", forState: .Normal)
+        self.saveButton.addTarget(self, action: "saveButtonPressed", forControlEvents: .TouchUpInside)
         self.view.addSubview(self.saveButton)
         
         self.imageView.snp_makeConstraints { (make) -> Void in
@@ -62,5 +66,88 @@ class ImagePreviewViewController: UIViewController {
     
     @objc private func cancelButtonPressed() {
         self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @objc private func saveButtonPressed() {
+        let assetsLibrary = ALAssetsLibrary()
+        let orientation : ALAssetOrientation = ALAssetOrientation(rawValue: UIImage().imageOrientation.rawValue)!
+
+        if let image = self.image {
+            assetsLibrary.writeImageToSavedPhotosAlbum(
+                image.CGImage,
+                orientation: ALAssetOrientation(rawValue: image.imageOrientation.rawValue)!,
+                completionBlock: { (url: NSURL!, error: NSError!) -> Void in
+                    assetsLibrary.assetForURL(
+                        url,
+                        resultBlock: { (asset: ALAsset!) -> Void in
+                            var albumCreated = false
+                            
+                            assetsLibrary.enumerateGroupsWithTypes(
+                                ALAssetsGroupAlbum,
+                                usingBlock: { (assetsGroup: ALAssetsGroup!, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+                                    // On the last iteration (when group is nil), we check if the album has been created
+                                    if (assetsGroup == nil && !albumCreated) {
+                                        assetsLibrary.addAssetsGroupAlbumWithName(
+                                            self.savedPhotosAlbumName,
+                                            resultBlock: { (assetsGroup: ALAssetsGroup!) -> Void in
+                                                self.imageFinishedSaving(nil)
+                                            },
+                                            failureBlock: { (error: NSError!) -> Void in
+                                                self.imageFinishedSaving(error)
+                                            }
+                                        )
+                                        
+                                        return
+                                    } else {
+                                        if let assetsGroup = assetsGroup {
+                                            let albumName = assetsGroup.valueForProperty(ALAssetsGroupPropertyName) as? String
+                                            if (albumName != nil)  {
+                                                if (albumName == self.savedPhotosAlbumName) {
+                                                    assetsGroup.addAsset(asset)
+                                                    stop.memory = true
+                                                    self.imageFinishedSaving(nil)
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                                failureBlock: { (error: NSError!) -> Void in
+                                    
+                                }
+                            )
+                        },
+                        failureBlock: { (error: NSError!) -> Void in
+                            
+                        }
+                    )
+                }
+            )
+        }
+    }
+    
+    private func imageFinishedSaving(error: NSError!) {
+        if (error != nil) {
+            
+        } else {
+            if let presentingViewController = self.presentingViewController {
+                presentingViewController.dismissViewControllerAnimated(true, completion: nil)
+                
+                let alertViewController = NYAlertViewController()
+                alertViewController.title = NSLocalizedString("Wallpaper Saved", comment: "")
+                alertViewController.message = NSLocalizedString("Your wallpaper was saved to the 'LockMinder Wallpapers' album in the Photos app", comment: "")
+                
+                let cancelAlertAction = NYAlertAction(
+                    title: NSLocalizedString("Wallpaper Saved", comment: ""),
+                    style: .Cancel,
+                    handler: { (action: NYAlertAction!) -> Void in
+                        presentingViewController.dismissViewControllerAnimated(true, completion: nil)
+                    }
+                )
+                
+                alertViewController.addAction(cancelAlertAction)
+                
+                presentingViewController.presentViewController(alertViewController, animated: true, completion: nil)
+            }
+        }
     }
 }
